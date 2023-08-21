c9b2ddfa9c60baa087886de5066f5ecb26d3f738
임성채 <lschmhj@naver.com>
2023-08-21 16:39:59 +0900

    Initial commit

---
+++ src/main/java/com/webtoonBoot/WebtoonBootApplication.java
@@ -0,0 +1,14 @@
+package com.webtoonBoot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoonBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtoonBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webtoonBoot/WebtoonBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtoonBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtoonBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
