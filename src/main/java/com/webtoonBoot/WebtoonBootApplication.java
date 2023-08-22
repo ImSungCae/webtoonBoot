7a15929675556f243a4396a405befb4a408436b2
임성채 <lschmhj@naver.com>
2023-08-22 14:51:45 +0900

    aws test
    aws test

--- src/main/java/com/webtoonBoot/WebtoonBootApplication.java
+++ src/main/java/com/webtoonBoot/WebtoonBootApplication.java
@@ -2,9 +2,18 @@ package com.webtoonBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
-public class WebtoonBootApplication {
+public class WebtoonBootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(WebtoonBootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebtoonBootApplication.class, args);
