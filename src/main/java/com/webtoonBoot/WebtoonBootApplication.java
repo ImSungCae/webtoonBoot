d2d7b41e0ec69034262c151d2429073261e7f881
임성채 <lschmhj@naver.com>
2023-08-22 14:57:15 +0900

    aws test1
    aws test1

--- src/main/java/com/webtoonBoot/WebtoonBootApplication.java
+++ src/main/java/com/webtoonBoot/WebtoonBootApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 @SpringBootApplication
 public class WebtoonBootApplication  {
-	
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebtoonBootApplication.class, args);
